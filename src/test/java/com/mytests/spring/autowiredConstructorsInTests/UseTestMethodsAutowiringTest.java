dde176c4e7d858511518d0969c2731ca182b91f5
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-12-01 13:12:58 +0100

    beans and junit-specific params in the test methods

--- src/test/java/com/mytests/spring/autowiredConstructorsInTests/UseTestMethodsAutowiringTest.java
+++ src/test/java/com/mytests/spring/autowiredConstructorsInTests/UseTestMethodsAutowiringTest.java
@@ -1,11 +1,12 @@
 package com.mytests.spring.autowiredConstructorsInTests;
 
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UseTestMethodsAutowiringTest {
@@ -20,5 +21,13 @@ public class UseTestMethodsAutowiringTest {
         assertNotNull(compo2);
         assertNull(nb);
 
+    }
+
+    // both beans and junit params
+    @RepeatedTest(2)
+    void placeOrderRepeatedly(RepetitionInfo repetitionInfo,
+                              @Autowired Compo1 compo1) {
+
+        assertEquals("c1", compo1.getId());
     }
 }
